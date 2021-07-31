a9d13882dc89ca05feaf9d5604b0ce47b7ea9e8f
Rayd Trujillo <rayd1893@gmail.com>
2021-07-31 06:28:10 +0000

    Update

---
+++ 0x00-math_complex/8-complex.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <math.h>
+
+double my_round(double number, int decimal)
+{
+	int ceros = pow(10, decimal);
+	double r;
+
+	r = (int) number * ceros;
+
+	return (r / ceros);
+}
+
+/**
+ * complex_from_mod_arg  - update the real and the imaginary parts
+ * @m: modulus
+ * @arg: argument
+ * @c3: complex number
+ *
+ * Return: void
+ */
+
+void complex_from_mod_arg(double m, double arg, complex *c3)
+{
+	double d;
+	double a;
+	double b;
+
+	d = tan(arg);
+	a = sqrt(pow(m, 2) / (1 + pow(d, 2)));
+	b = d * a;
+	c3->re = my_round(a,1);
+	c3->im = my_round(b,1);
+}
