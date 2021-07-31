ff4c7ccd430ca6442b733d738067225a40891186
Rayd Trujillo <rayd1893@gmail.com>
2021-07-31 05:02:30 +0000

    Change format print

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "holberton.h"
 
+int is_double(double number)
+{
+	int value = number;
+
+	if (value == number)
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
 /**
  * _abs - Absolute value
  * @number: Number
  *
  * Return: Always positive
  */
-int _abs(int number)
+double _abs(double number)
 {
 	if (number < 0)
 		return (number * -1);
@@ -27,7 +39,10 @@ void display_complex_number(complex c)
 {
 	if (c.re != 0)
 	{
-		printf("%d", c.re);
+		if (is_double(c.re))
+			printf("%.1f", c.re);
+		else
+			printf("%.0f", c.re);
 
 		if (c.im != 0)
 		{
@@ -40,7 +55,12 @@ void display_complex_number(complex c)
 			if (_abs(c.im) == 1)
 				printf("i");
 			else
-				printf("%di", _abs(c.im));
+			{
+				if (is_double(c.im))
+					printf("%.1fi", _abs(c.im));
+				else
+					printf("%.0fi", _abs(c.im));
+			}
 		}
 	}
 
@@ -62,7 +82,12 @@ void display_complex_number(complex c)
 			if (_abs(c.im) == 1)
 				printf("i");
 			else
-				printf("%di", _abs(c.im));
+			{
+				if (is_double(c.im))
+					printf("%.1fi", _abs(c.im));
+				else
+					printf("%.0fi", _abs(c.im));
+			}
 
 		}
 	}
