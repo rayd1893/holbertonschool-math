eb3d9110d8e3a2fc65bc780f407b6a8cd240a2a5
Rayd Trujillo <rayd1893@gmail.com>
2021-08-13 04:36:49 +0000

    Heron Sequence

---
+++ 0x01-math_sequence/heron.h
@@ -0,0 +1,23 @@
+#ifndef HERON_H
+#define HERON_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * Struct cell - Struct for sequence heron
+ * @elt: Number
+ * @next: struct type cell
+ *
+ * Description: List linked from cell
+ */
+typedef struct cell
+{
+	double elt;
+	struct cell *next;
+} t_cell;
+
+t_cell *heron(double p, double x0);
+
+#endif
